fe8b517e79ebc8b938db435f1fae97363f7a4b35
devon <yj545685@gmail.com>
2018-06-13 18:13:41 +0900

    시간계산로직 코드 추가

---
+++ src/time/DateCalculate.java
@@ -0,0 +1,62 @@
+package time;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateCalculate {
+	public static void main(String[] args) throws ParseException {
+		DateCalculate dc = new DateCalculate();
+		System.out.println(dc.isTimeout());
+	}
+
+	public void timeCalculator() throws ParseException {
+		// 요청시간
+		String reqDateStr = "201702011535";
+
+		// 포맷정하기
+		SimpleDateFormat dateFormat = new SimpleDateFormat("YYYYMMddHHmm");
+		// 현재시간 Date
+		Date curDate = new Date();
+
+		// 요청시간을 Date로 parsing 후 time가져오기
+		Date reqDate = dateFormat.parse(reqDateStr);
+		long reqDateTime = reqDate.getTime();
+
+		// 현재시간을 요청시간의 형태로 format 후 time 가져오기
+		curDate = dateFormat.parse(dateFormat.format(curDate));
+		long curDateTime = curDate.getTime();
+		// 분으로 표현
+		long minute = (curDateTime - reqDateTime) / 60000;
+		System.out.println("요청시간 : " + reqDate);
+		System.out.println("현재시간 : " + curDate);
+		System.out.println(minute + "분 차이");
+
+	}
+
+	public boolean isTimeout() throws ParseException {
+
+		// 요청시간
+		String reqDateStr = "201702011535";
+
+		// 현재시간 Date
+		SimpleDateFormat dateFormat = new SimpleDateFormat("YYYYMMddHHmm");
+		Date reqDate = dateFormat.parse(reqDateStr);
+
+		Calendar cal = Calendar.getInstance();
+		// 1일이 경과했는지 확인
+		// 일단위: cal.add(Calendar.DATE, 며칠*-1);
+		// 시간단위: cal.add(Calendar.HOUR, 몇시간*-1);
+		int date = 2; // 2일경과
+		// 현재시간에서 2일 전 계산
+		cal.add(Calendar.DATE, date * -1);
+		Date comparedTime = cal.getTime();
+
+		// 현재시간에서 2일전과 비교시간(reqDateStr) 비교하여 초과했으면 시간경과!
+		if (comparedTime.compareTo(reqDate) > 0) {
+			return false;
+		}
+		return true;
+	}
+}
